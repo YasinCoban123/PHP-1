6805750afcf576b855fa50c7dec54d3ecf1b1377
YasinCoban123 <9018681@student.zadkine.nl>
2022-11-22 11:55:24 +0100

    yy

---
+++ opdracht3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+$x = "Yasin Coban";
+$y = 'Klas=1G';
+
+echo $x;
+echo "<br>"; 
+echo $y;
+echo "<br>";
+$naam = "Yasin Coban !";
+
+echo "$naam";
+echo "<br>";
+$denk = "numberssss";
+
+echo $denk;
+echo "<br>";
+?>
+
+</body>
+</html>
